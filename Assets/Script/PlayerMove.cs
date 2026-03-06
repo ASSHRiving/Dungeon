70194096df6f1581c326482151e4d3b2de961a0f
Rich <ky2244@gmail.com>
2026-03-06 14:50:13 +0800

    新增角色wasd移動

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class PlayerControl
+public class PlayerMove : MonoBehaviour
 {
-    
+    public float speed = 5f;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal"); // A D
+        float z = Input.GetAxis("Vertical");   // W S
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
 }
