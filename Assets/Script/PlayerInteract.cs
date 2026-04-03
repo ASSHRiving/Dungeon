a5875ff95825e80e5c81165207d55d44e5f4dc7f
Rich <ky2244@gmail.com>
2026-04-03 16:55:39 +0800

    修改第三人稱交互邏輯為二段式偵測

--- Assets/Script/PlayerInteract.cs
+++ Assets/Script/PlayerInteract.cs
@@ -3,20 +3,38 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     public float interactDistance = 5f;
+    public Transform interactOrigin; // 建議設在角色胸口或頭
+    public LayerMask interactMask;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) //按e
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            // ① 從畫面中心取得目標點
+            Ray cameraRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit camHit;
 
+            Vector3 targetPoint;
+
+            if (Physics.Raycast(cameraRay, out camHit, 100f, interactMask))
+            {
+                targetPoint = camHit.point;
+            }
+            else
+            {
+                targetPoint = cameraRay.GetPoint(100f);
+            }
+
+            // ② 從角色發射 Ray（比較合理）
+            Vector3 dir = (targetPoint - interactOrigin.position).normalized;
+
+            Ray playerRay = new Ray(interactOrigin.position, dir);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, interactDistance))
+            if (Physics.Raycast(playerRay, out hit, interactDistance))
             {
                 Debug.Log("Hit: " + hit.collider.name);
 
-                // 找互動物件
                 IInteractable interactable = hit.collider.GetComponent<IInteractable>();
 
                 if (interactable != null)
@@ -24,6 +42,11 @@ public class PlayerInteract : MonoBehaviour
                     interactable.Interact();
                 }
             }
+
+            // Debug
+            Debug.DrawRay(cameraRay.origin, cameraRay.direction * 100f, Color.red);
+            Debug.DrawRay(interactOrigin.position, dir * interactDistance, Color.green);
+            Debug.Log("Dir: " + dir);
         }
     }
 }
