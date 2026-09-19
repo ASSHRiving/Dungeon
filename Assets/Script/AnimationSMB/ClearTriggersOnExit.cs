d62bfb4e2412fe7e4ddb2ebbf44d1959b8e78b52
Rich <ky2244@gmail.com>
2026-09-20 02:33:53 +0800

    將受擊後的狀態重製全部移至動畫腳本

--- Assets/Script/AnimationSMB/ClearTriggersOnExit.cs
+++ Assets/Script/AnimationSMB/ClearTriggersOnExit.cs
@@ -8,6 +8,12 @@ public class ClearTriggersOnExit : StateMachineBehaviour
     // 當離開受傷動畫狀態時觸發
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        CharacterCombatBase combat = animator.GetComponent<CharacterCombatBase>();
+        if(combat != null)
+        {
+            combat.canAttack = true;
+            combat.currentWeapon.combo = 0;
+        }
         foreach (string triggerName in triggerNamesToReset)
         {
             animator.ResetTrigger(triggerName);
--- Assets/Script/Player/PlayerHealthSystem.cs
+++ Assets/Script/Player/PlayerHealthSystem.cs
@@ -53,11 +53,6 @@ public class PlayerHealthSystem : CharacterHealthBase
             if(currentPoise <= 0)
             {
                 _animator.CrossFade(attackData.hitAnimationName, 0.1f);
-                _animator.ResetTrigger("Roll");
-                _animator.ResetTrigger("LAtk");
-                _combat.canAttack = true;
-                _combat.currentWeapon.combo = 0;
-
                 currentPoise = maxPoise;
             }     
         }
